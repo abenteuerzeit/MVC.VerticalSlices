3b59a182343a36a3f16c70c9e4567a332c0297c8
abenteuerzeit <dr.adrian.mroz@outlook.com>
2024-06-30 20:51:14 +0200

    🧱 Update view engine paths

---
+++ MVC.VerticalSlices/FeatureViewLocationRazorViewEngine.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace MVC.VerticalSlices
+{
+    public class FeatureFolderViewLocationExpander : IViewLocationExpander
+    {
+        public string[] FeatureLocations { get; }
+
+        public string[] OldViewLocations { get; } = ["~/Views/{1}/{0}.cshtml", "~/Views/Shared/{0}.cshtml"];
+
+        public FeatureFolderViewLocationExpander()
+        {
+            FeatureLocations = ["~/Features/{1}/{0}.cshtml", "~/Features/Shared/Views/{0}.cshtml"];
+        }
+
+
+        public FeatureFolderViewLocationExpander(string[] customLocations)
+        {
+            FeatureLocations = customLocations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            return [.. FeatureLocations, .. OldViewLocations];
+        }
+    }
+}
--- MVC.VerticalSlices/Program.cs
+++ MVC.VerticalSlices/Program.cs
@@ -1,36 +1,43 @@
-namespace MVC.VerticalSlices
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace MVC.VerticalSlices;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        // Register custom view location expander
+        builder.Services.Configure<RazorViewEngineOptions>(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            options.ViewLocationExpanders.Add(new FeatureFolderViewLocationExpander());
+        });
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+        var app = builder.Build();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
